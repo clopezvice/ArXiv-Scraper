9da4288b3e1f73a45ecddf9282e4d535555f4cad
clopezvice <clopezvice@uoc.edu>
2025-11-05 00:08:01 +0100

    Update beascraper.py

--- arxivscraper/webtools/beascraper.py
+++ arxivscraper/webtools/beascraper.py
@@ -10,7 +10,7 @@ from a given URL, handles possible HTTP errors, and returns a parsed BeautifulSo
 
 Authors:
     - Alejandro Cano Jones (acanojo@uoc.edu)
-    - Christian López (clopezvice@uoc.edu)
+    - Christian López Vicente (clopezvice@uoc.edu)
 
 Subject:
     M2.851 - Tipología y ciclo de vida de los datos. Master in Data Science (UOC)
